d77d1c5acf069ab9c75b5bbd2212c3282656beba
JBUkim <91812103@jmail.ac.kr>
2024-06-11 13:10:15 +0900

    lunch

--- app.py
+++ app.py
@@ -1,56 +1,71 @@
 import json
 
+import tool
+
 from flask import Flask, request, render_template
 from flask_cors import CORS
-
-import tool
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def index():
-    filenames = [i for i in range(1, 26)] # 1 ~ 25
+@app.get('/')
+def lunch_form():
+    meal_options = ['식사', '요리', '간식']
+    cuisine_options = ['한식', '중식', '일식', '양식', '아시아']
+    situation_options = ['혼밥', '친구', '연인', '가족', '모임']
     
-    return render_template('./index.html', filenames=filenames) # html template으로 데이터 전달
+    return render_template('lunch.html', meal_options=meal_options, cuisine_options=cuisine_options, situation_options=situation_options)
 
-# http://localhost:5000/
+
+
+# http://localhost:5000/lunch
 @app.post('/')
-def movie_proc():
-    data = request.json
-    movie= data['movie']
-    
-    movie= movie.split(',')
-    print('-> movie:', movie)
-    
-    # movie 배열의 요소를 정수로 변경하여 list로 변경
-    movie= list(map(int, movie)) # map: 배열의 요소에 함수를 적용하는 기능을 함.
-    print('-> movie:', movie)
+def lunch_proc():
+    try:
+        data = request.json
+        lunch = data['lunch']
+        
+        lunch = lunch.split(',')
+        print('-> lunch:', lunch)
+        
+        lunch = list(map(int, lunch))
+        print('-> lunch:', lunch)
 
-    # return movie
-    
-    movies = ['반지의 제왕', 'A Quiet Place (2018)', '러브액츄얼리', '화이트 칙스', 'Interstellar (2014)',
-            '해리포터와 마법사의 돌', 'The Autopsy of Jane Doe (2016)', '타이타닉', '세 얼간이', 'A.I. (2001)',
-            '캐리비안의 해적', 'The Conjuring (2013)', '맘마미아', '덤 앤 더머', 'The Martian (2015)',
-            '닥터 스트레인지', 'The Exorcist (1973)', 'La La Land (2016)', '우리는 동물원을 샀다.', 'Edge of Tomorrow (2014)',
-            '아바타 (2009)', 'The Rite (2011)', '비긴 어게인', '미트 페어런츠', 'Gravity (2013)']
-    
-    watch = []
-    for index in range(len(movie)): # 0 ~ 24
-        if movie[index] == 1: # 시청한 영화이면
-            watch.append(movies[index]) # 시청한 영화의 이름을 추가
+        meal_types = ['식사', '요리', '간식']
+        cuisines = ['한국', '중국', '일본', '서양', '아시아']
+        situations = ['혼자', '친구와', '연인과', '가족과', '모임에서']
+        
+        # 사용자가 선택한 카테고리 정보를 조합하여 프롬프트 생성
+        selected_meal_types = [meal_types[index] for index, value in enumerate(lunch[:3]) if value == 1]
+        selected_cuisines = [cuisines[index] for index, value in enumerate(lunch[3:8]) if value == 1]
+        selected_situation = [situations[index] for index, value in enumerate(lunch[8:13]) if value == 1]
+        selected_situation_str = selected_situation[0] if selected_situation else ''  # 선택된 상황이 없는 경우를 처리
+        
+        prompt = f'내가 지금 먹고 싶은건 {", ".join(selected_meal_types)} 이야. 그리고 이 음식은 {", ".join(selected_cuisines)} 음식중에서 골라줘. '
+        prompt += f'{"지금 상황은 " + selected_situation_str + " 먹여야 하는 상황이야." if selected_situation_str else ""} '
+        prompt += '위의 상황들을 종합해서 메뉴 1개를 추천해줘.'
+        prompt += '추천한 메뉴의 레시피를 5줄로 출력해줘.'
 
-    watch_join = ','.join(watch) # 시청한 영화를 ","로 구분
-    
-    print('->watch_join:', watch_join)
-    # return watch_join  # 콰이어트 플레이스,제인도,컨저링,엑소시스트,더 라이트
+        print('-> prompt:', prompt)
+        
+        format = '''{"res":"음식 추천", "recipe":"레시피 내용"}'''  # 레시피 내용은 실제 레시피로 대체되어야 합니다.
 
-    prompt = f'내가 시청한 영화는 [{watch_join}]이야, 내가 시청한 장르의 영화 중 2000년 이후에 출시되고, 내가 시청하지 않고 평점이 높은 영화 5편을 추천해줘.'
-    print('-> prompt:', prompt)
-    
-    format = '''{"res":"영화 목록"}'''
+        response = tool.answer('너는 음식 추천 시스템이야.', prompt, format)
+        print('-> response:', response)
+        
+        return response
+    except Exception as e:
+        print('Error:', str(e))
+        return {'error': str(e)}, 500
 
-    response = tool.answer('너는 영화 추천 시스템이야.', prompt, format)
-    print('-> response:', response)
-    
-    return response  # json 객체 전달
+
+
+
+
+
+app.run(host="0.0.0.0", port=5000, debug=True)  # 0.0.0.0: 모든 Host 에서 접속 가능, debug=True: 소스 변경시 자동 restart
+
+'''
+activate ai
+python lunch.py
+'''
